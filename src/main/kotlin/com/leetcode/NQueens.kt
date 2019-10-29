9f9e0129ee0d265106082186274baf8c4a479476
unrealwork <unrealwork@gmail.com>
2019-10-29 23:57:43 +0300

    qp

--- src/main/kotlin/com/leetcode/NQueens.kt
+++ src/main/kotlin/com/leetcode/NQueens.kt
@@ -4,75 +4,46 @@ import java.util.*
 
 class NQueens {
     fun solveNQueens(n: Int): List<List<String>> {
-        val board = Board(n, 0)
+        val board = Board(n, emptySet())
         val boards = Stack<Board>()
         boards.addAll(board.next())
         val covered = mutableSetOf<Board>()
         while (!boards.empty()) {
             val bo = boards.pop()
             if (!covered.contains(bo)) {
-                val next = board.next()
+                val next = bo.next()
                 next.forEach { b -> boards.push(b) }
                 covered.add(bo)
             }
         }
-        return covered.filter { b -> b.next()                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                              .isEmpty() && b.queens == n }.map { b -> b.toAnswer() }
+        return covered.filter { b -> b.queens.size == n }
+                .map { b -> b.toAnswer() }
     }
 }
 
-class Board(val size: Int, val queens:Int) {
-    // TODO: Store only queens positions
-    private var grid = Array(size) { IntArray(size) { -1 } }
-    private var nextBoards: Set<Board>? = null
-
-    constructor(other: Board) : this(other.size, other.queens) {
-        this.grid = other.grid
-    }
-
+class Board(val size: Int, val queens: Set<QueenPosition>) {
+    private var nextBoards: List<Board>? = null;
     fun toAnswer(): List<String> {
-        return grid.map { ba -> ba.map { b -> if (b == 1) 'Q' else if (b == 0) '+' else '.' }.toCharArray() }
-            .map { ca -> String(ca) }
-            .toList()
+        return queens
+                .sortedBy { queenPosition -> queenPosition.i }
+                .map { queenPosition ->
+                    val chars = CharArray(size) { '.' }
+                    chars[queenPosition.j] = 'Q'
+                    String(chars)
+                }.toList()
     }
 
-    fun next(qi: Int, qj: Int): Board {
-        val res = Board(size, queens + 1)
-        res.grid = this.grid.map { b -> b.clone() }.toTypedArray()
-        res.grid[qi][qj] = 1
-        val diff = qi - qj
-        val sum = qi + qj
-        for (i in 0 until size) {
-            for (j in 0 until size) {
-                if (i != qi || j != qj || res.grid[i][j] == -1) {
-                    if (qi == i) {
-                        res.grid[i][j] = 0
-                    }
-                    if (qj == j) {
-                        res.grid[i][j] = 0
-                    }
-                    if (i - j == diff) {
-                        res.grid[i][j] = 0
-                    }
-                    if (i + j == sum) {
-                        res.grid[i][j] = 0
-                    }
-                }
-            }
-        }
-        return res
+    fun next(np: QueenPosition): Board {
+        return Board(size, queens + np)
     }
 
-    fun next(): Set<Board> {
+    fun next(): List<Board> {
         if (nextBoards == null) {
-           val res = mutableSetOf<Board>()
-            for (i in 0 until size) {
-                for (j in 0 until size) {
-                    if (this.grid[i][j] == -1) {
-                        res.add(next(i, j))
-                    }
-                }
-            }
-            nextBoards = res
+            nextBoards = (0 until size)
+                    .flatMap { i -> (0 until size).map { j -> (IndexedValue(i, j)) } }
+                    .map { (i, j) -> QueenPosition(i, j) }
+                    .filter { np -> queens.none { qp -> qp.intercepts(np) } }
+                    .map { np -> next(np) }
         }
         return nextBoards!!
     }
@@ -84,14 +55,40 @@ class Board(val size: Int, val queens:Int) {
         other as Board
 
         if (size != other.size) return false
-        if (!grid.contentDeepEquals(other.grid)) return false
+        if (queens != other.queens) return false
 
         return true
     }
 
     override fun hashCode(): Int {
         var result = size
-        result = 31 * result + grid.contentDeepHashCode()
+        result = 31 * result + queens.hashCode()
+        return result
+    }
+
+}
+
+data class QueenPosition(val i: Int, val j: Int) {
+    fun intercepts(other: QueenPosition): Boolean {
+        return (other.i == i) || (other.j == j)
+                || (i - j == other.i - other.j) || (i + j == other.i + other.j)
+    }
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as QueenPosition
+
+        if (i != other.i) return false
+        if (j != other.j) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        var result = i
+        result = 31 * result + j
         return result
     }
 
@@ -100,6 +97,6 @@ class Board(val size: Int, val queens:Int) {
 
 fun main(args: Array<String>) {
     val solution = NQueens()
-    val res = solution.solveNQueens(4)
+    val res = solution.solveNQueens(5)
     println(res)
 }
