9ba59dbd70be13180f1ad27b83cfde6837cf1337
unrealwork <unrealwork@gmail.com>
2019-10-31 02:32:00 +0300

    contains duplicate

---
+++ src/main/kotlin/com/leetcode/ContainsDuplicate.kt
@@ -0,0 +1,19 @@
+package com.leetcode
+
+class ContainsDuplicate {
+    fun containsDuplicate(nums: IntArray): Boolean {
+        for ((i,    n) in nums.withIndex()) {
+            for (j in 0 until i) {
+                if (nums[j] == n) {
+                    return true
+                }
+            }
+        }
+        return false
+    }
+}
+
+fun main(args: Array<String>) {
+    val solution = ContainsDuplicate()
+    println(solution.containsDuplicate(intArrayOf(1,2,3,1)))
+}
